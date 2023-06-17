49b954ed2d3f386de95a89f2dfd6830008cf3270
phant80m <black.pandazayd@gmail.com>
2023-06-17 20:24:40 +1000

    complete overhall

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// nothing here
+
+$(document).ready(function() {
+  var hidden, visibilityState, visibilityChange;
+
+  if (typeof document.hidden !== "undefined") {
+    hidden = "hidden", visibilityChange = "visibilitychange", visibilityState = "visibilityState";
+  } else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden", visibilityChange = "msvisibilitychange", visibilityState = "msVisibilityState";
+  }
+
+  var document_hidden = document[hidden];
+
+  document.addEventListener(visibilityChange, function() {
+    if(document_hidden != document[hidden]) {
+      if(document[hidden]) {
+        // Document hidden
+        document.title = "phant80m's repo | add to sileo"
+      } else {
+        // Document shown
+        document.title = "phant80m repo"
+      }
+
+      document_hidden = document[hidden];
+    }
+  });
+});
+
+$(document).ready(function() {
+  if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    $('#sileo').html('Add to sileo');
+  } else {
+    $('#sileo').html('Not available on your device');
+  }
+});
+
+$('#sileo').on('click', function(event) {
+  if (!/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    event.preventDefault();
+    alert('Not available on your device');
+  }
+});
